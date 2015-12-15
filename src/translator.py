66151b7ed6ed779da6e249d142d027635b966389
Vu Phan <vuphan314@gmail.com>
2015-12-14 18:11:42 -0600

    Updated guide

--- src/translator.py
+++ src/translator.py
@@ -5,22 +5,32 @@ To run 'translator' via a command-line interface:
     1.  Change the directory to src.
     2.  Provide an input L program and a location for the output ASP program:
             python translator.py <path_to_L_program> > <path_to_ASP_program>
-        Warning: If the output file already exists, then it will be overwritten.
-    3.  Call SPARC:
-            java -jar sparc.jar <path_to_ASP_program>
-    4.  Make a query about the logic program:
+        WARNING: If the output file already exists, then it will be overwritten.
+    3'. (Answer set mode)
+        Call SPARC:
+            java -jar sparc.jar -solver clingo -A <path_to_ASP_program>
+    3". (Querying mode)
+        Call SPARC:
+            java -jar sparc.jar -solver clingo <path_to_ASP_program>
+        Make a query about the logic program:
             <query>
+        To quit:
+            exit
 
 An example, with Windows Command Prompt:
-    1.  Type:
+    1.  Enter:
             cd/d E:\LtoASPtranslator\src
-    2.  Type:
+    2.  Enter:
             python translator.py examples/ch5.l > tmp/ch5.sp
         (Python 3.4.3)
-    3.  Type:
-            java -jar sparc.jar tmp/ch5.sp
-    4.  Type:
+    3'. Enter:
+            java -jar sparc.jar -solver clingo -A tmp/ch5.sp
+    3". Enter:
+            java -jar sparc.jar -solver clingo tmp/ch5.sp
+        Enter:
             safety_reqts_fully_realised
+        Enter:
+            exit
 '''
 
 ########## ########## ########## ########## ########## ########## ########## ##########
